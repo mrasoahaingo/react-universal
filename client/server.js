f982a9007ecbbeaba1a3d31f6982a9ac1127e589
Michaël Rasoahaingo <michael.rasoahaingo@gmail.com>
2016-04-05 09:30:22 +0200

    Add universal

--- client/server.js
+++ client/server.js
@@ -1,29 +1,30 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('../webpack.config');
+import express from 'express'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from '../webpack.config'
 
-var app = express();
-var compiler = webpack(config);
+const app = express()
+const compiler = webpack(config)
 
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}));
+}))
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(webpackHotMiddleware(compiler))
 
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public'))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'))
+})
 
-app.listen(3000, function(err) {
+app.listen(3000, (err) => {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
 
-  console.log('Listening at http://localhost:3000');
-});
+  console.log('Listening at http://localhost:3000')
+})
--- server/server.js
+++ server/server.js
@@ -12,17 +12,17 @@ import { createStore } from 'redux'
 import liveApp from '../common/reducers'
 import AppContainer from '../common/containers/App'
 
-let app = express();
-let compiler = webpack(config);
+const app = express()
+const compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}));
+}))
 
-app.use(webpackHotMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler))
 
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public'))
 
 // This is fired every time the server side receives a request
 app.use((req, res) => {
@@ -60,9 +60,9 @@ app.use((req, res) => {
 
 app.listen(3000, (err) => {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
 
-  console.log('Listening at http://localhost:3000');
-});
+  console.log('Listening at http://localhost:3000')
+})
