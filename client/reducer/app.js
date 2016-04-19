ad54f862a8445719066d59512a5120e1647fc62a
Michaël Rasoahaingo <michael.rasoahaingo@gmail.com>
2016-04-19 14:45:31 +0200

    Update react

--- client/reducer/app.js
+++ client/reducer/app.js
@@ -2,7 +2,7 @@ export default (state = {}, {type, data}) => {
   switch (type) {
     case 'UPDATE_STATE':
       console.log('[BROWSER] reduce app state', Object.assign({}, state, data.app))
-      return Object.assign({}, state, data.app)
+      return {...state, ...data.app}
     default:
       return state
   }
